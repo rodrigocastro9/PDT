207babb63e96073ab8768e31c850d453f0f3288d
Damian <d-bozz@hotmail.com>
2019-10-12 17:33:14 -0300

    Se agrega entidad Localidad

--- ejbModule/com/entidades/Caracteristica.java
+++ ejbModule/com/entidades/Caracteristica.java
@@ -6,19 +6,9 @@
 package com.entidades;
 
 import java.io.Serializable;
-import java.util.List;
-
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Stateless
---
+++ ejbModule/com/entidades/Localidad.java
@@ -0,0 +1,87 @@
+package com.entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Localidad
+ *
+ */
+@Entity
+@Table(name = "LOCALIDAD")
+public class Localidad implements Serializable {
+
+	
+	public Localidad() {
+		super();
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	@Id
+    @GeneratedValue( strategy = GenerationType.AUTO )
+	@Column(name="ID_LOCALIDAD")
+	private long id;	
+	@ManyToOne (optional=false)
+	private Departamento departamento; 
+	@Column(name="NOMBRE_LOC",length=(40),nullable=false)
+	private String nombreLoc;
+	@Column(name="LATITUD",length=(50),nullable=false)
+	private float latitud;
+	@Column(name="LONGITUD",length=(50),nullable=false)
+	private float longitud;
+	@Column(name="ALTITUD",length=(50),nullable=false)
+	private String altitud;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+	public String getNombreLoc() {
+		return nombreLoc;
+	}
+	public void setNombreLoc(String nombreLoc) {
+		this.nombreLoc = nombreLoc;
+	}
+	public float getLatitud() {
+		return latitud;
+	}
+	public void setLatitud(float latitud) {
+		this.latitud = latitud;
+	}
+	public float getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(float longitud) {
+		this.longitud = longitud;
+	}
+	public String getAltitud() {
+		return altitud;
+	}
+	public void setAltitud(String altitud) {
+		this.altitud = altitud;
+	}
+	public Localidad(long id, Departamento departamento, String nombreLoc, float latitud, float longitud,
+			String altitud) {
+		super();
+		this.id = id;
+		this.departamento = departamento;
+		this.nombreLoc = nombreLoc;
+		this.latitud = latitud;
+		this.longitud = longitud;
+		this.altitud = altitud;
+	}
+	
+}
+	
+
